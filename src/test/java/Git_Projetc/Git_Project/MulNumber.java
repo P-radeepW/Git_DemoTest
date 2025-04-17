162ad103329a43ded42f9b7c75750ddc47960814
Pradeep <wagh.pradeep2923@gmail.com>
2025-04-17 23:48:53 +0530

    Mul Number Class

---
+++ src/test/java/Git_Projetc/Git_Project/MulNumber.java
@@ -0,0 +1,17 @@
+package Git_Projetc.Git_Project;
+
+public class MulNumber 
+{
+	public static void main(String args[])
+	{
+		mul();
+	}
+	
+	public static void mul()
+	{
+		int a=2,b=5;
+		int c=a*b;
+		System.out.println(c);
+	}
+
+}
