2edb438c21269b5172c5793f42d994dcd76efe02
Pradeep <wagh.pradeep2923@gmail.com>
2025-04-17 22:57:25 +0530

    Sub Number File

---
+++ src/test/java/Git_Projetc/Git_Project/SubNumber.java
@@ -0,0 +1,19 @@
+package Git_Projetc.Git_Project;
+
+public class SubNumber 
+{
+	
+	public static void main(String args[])
+	{
+		sub();
+	}
+
+	public static void sub()
+	{
+		int a=10,b=3;
+		int c=0;
+		c=a-b;
+		System.out.println(c);
+	}
+	
+}
