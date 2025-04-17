6e4f4c31810468d5467ce5ef587d5fed33ae5f83
Pradeep <wagh.pradeep2923@gmail.com>
2025-04-17 23:55:22 +0530

    Div Number File

---
+++ src/test/java/Git_Projetc/Git_Project/DivNum.java
@@ -0,0 +1,18 @@
+package Git_Projetc.Git_Project;
+
+public class DivNum 
+{
+
+	public static void main(String args[])
+	{
+		div();
+	}
+	
+	public static void div()
+	{
+		int a=2,b=5;
+		int c=b/a;
+		System.out.println(c);
+	}
+	
+}
