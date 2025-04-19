774cb831ea0c737c2c30778f46d0f1d08df5c2dc
Pradeep <wagh.pradeep2923@gmail.com>
2025-04-19 11:44:30 +0530

    Reverse number

---
+++ src/test/java/Git_Projetc/Git_Project/RevNum.java
@@ -0,0 +1,15 @@
+package Git_Projetc.Git_Project;
+
+public class RevNum 
+{
+	public static void main(String args[])
+	{
+		
+	}
+
+	public static void revNumber()
+	{
+		int a=10;
+		int d=20;
+	}
+}
